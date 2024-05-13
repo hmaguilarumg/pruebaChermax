21bd8c7d603a87099d0dbcf078080b5e50a75efc
hmaguilarumg <HMVAguilarO@chncentral.chn.com.gt>
2024-05-13 15:58:58 -0600

    prueba

---
+++ Downloads/web-empagua/src/main/java/gt/com/chn/webempagua/WebEmpaguaApplication.java
@@ -0,0 +1,13 @@
+package gt.com.chn.webempagua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEmpaguaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebEmpaguaApplication.class, args);
+	}
+
+}
